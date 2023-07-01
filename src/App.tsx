c7ebd3746d054023a92037b44edc1344c02fae81
nadav ninay <nadavninay6@gmail.com>
2023-07-01 21:19:41 +0300

    comments

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,7 @@ function App() {
         </Alert>
       </Snackbar>
       {loading ? (
+        // Loading Spinner
         <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
           <CircularProgress />
         </Box>
