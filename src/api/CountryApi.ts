f6bdb35364ba54ab1e091a9ca16047a16740571b
nadav ninay <nadavninay6@gmail.com>
2023-07-02 12:11:49 +0300

    AXIOS command

--- src/api/CountryApi.ts
+++ src/api/CountryApi.ts
@@ -4,6 +4,7 @@ import { Country } from "../types/Country";
 export const getCountryData = async (
   countryName: string
 ): Promise<Country[]> => {
+  //use at axios to get country from API
   const response = await axios.get<Country[]>(
     `https://restcountries.com/v3.1/name/${countryName}`
   );
